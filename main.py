2ac7963fd3474e05df0c80f2fb358d951f61772a
OAVallejos <135759800+OAVallejos@users.noreply.github.com>
2023-10-26 15:18:49 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hola Mundo")
+from fastapi import FastAPI
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, query_param: str = None):
+    return {"item_id": item_id, "query_param": query_param}
