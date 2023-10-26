7a7bfc6cee6ed9c13fd5e463f1cd080d643f8b36
OAVallejos <135759800+OAVallejos@users.noreply.github.com>
2023-10-26 12:26:08 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,73 +1,54 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-# main.py
-
-if __name__ == "__main__":
-    print("La aplicación se está ejecutando correctamente.")
-    # Aquí puedes agregar el código principal de tu aplicación
+import nltk
+from nltk.chat import Chat
+from nltk.corpus import stopwords
+from spacy import displacy
 
 
+# Configuración de NLTK
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')
 
-# Datos de ejemplo (reemplázalos con tus datos reales)
-data = {
-    "games": [
-        {
-            "title": "Game A",
-            "genre": "Action",
-            "year": 2012,
-            "playtime": 150
-        },
-        {
-            "title": "Game B",
-            "genre": "Adventure",
-            "year": 2013,
-            "playtime": 200
-        },
-        # Agrega más datos aquí
-    ]
-}
+from typing_extensions import deprecated
 
-# Define tus funciones de endpoints aquí
-# Nota: Debes implementar las funciones con lógica real según tus datos
+deprecated = None  # silence the warning
 
-@app.get("/")
-def read_root():
-    return {"message": "¡API de la empresa!"}
+# Configuración de spaCy
+nlp = displacy(model='en_core_web_sm')
 
-@app.get("/playtime_genre/{genre}")
-def playtime_genre(genre: str):
-    # Implementa la lógica para encontrar el año con más horas jugadas para el género dado
-    # Devuelve un diccionario con el resultado
-    # Ejemplo de retorno: {"Año de lanzamiento con más horas jugadas para Género X" : 2013}
-    pass
+# Función para limpiar el texto
+def clean_text(text):
+    text = text.lower()
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    text = " ".join(word for word in text.split() if word.isalpha())
+    return text
 
-@app.get("/user_for_genre/{genre}")
-def user_for_genre(genre: str):
-    # Implementa la lógica para encontrar el usuario con más horas jugadas para el género dado
-    # y una lista de acumulación de horas jugadas por año
-    # Devuelve un diccionario con el resultado
-    # Ejemplo de retorno: {"Usuario con más horas jugadas para Género X" : us213ndjss09sdf, "Horas jugadas":[{Año: 2013, Horas: 203}, {Año: 2012, Horas: 100}, {Año: 2011, Horas: 23}]}
-    pass
+# Función para extraer el significado de las palabras
+def get_meaning(word):
+    try:
+        return wordnet.synonyms(word, pos=1)[0]
+    except:
+        return "No se encontró un significado para la palabra"
 
-@app.get("/users_recommend/{year}")
-def users_recommend(year: int):
-    # Implementa la lógica para encontrar el top 3 de juegos MÁS recomendados por usuarios para el año dado
-    # Devuelve una lista de diccionarios con el resultado
-    # Ejemplo de retorno: [{"Puesto 1" : X}, {"Puesto 2" : Y},{"Puesto 3" : Z}]
-    pass
+# Función para crear el chatbot
+def chatbot(input_text):
+    input_text = clean_text(input_text)
+    words = nltk.word_tokenize(input_text)
+    response = ""
+    for word in words:
+        if word in stopwords.words('english'):
+            continue
+        else:
+            meaning = get_meaning(word)
+            if meaning:
+                response += f"{word} significa {meaning}.\n"
+            else:
+                response += f"No se encontró un significado para la palabra {word}.\n"
+    return response
 
-@app.get("/users_not_recommend/{year}")
-def users_not_recommend(year: int):
-    # Implementa la lógica para encontrar el top 3 de juegos MENOS recomendados por usuarios para el año dado
-    # Devuelve una lista de diccionarios con el resultado
-    # Ejemplo de retorno: [{"Puesto 1" : X}, {"Puesto 2" : Y},{"Puesto 3" : Z}]
-    pass
+# Creación del chatbot
+chat = Chat(chatbot)
 
-@app.get("/sentiment_analysis/{year}")
-def sentiment_analysis(year: int):
-    # Implementa la lógica para el análisis de sentimiento según el año de lanzamiento
-    # Devuelve un diccionario con la cantidad de registros de reseñas de usuarios categorizados con análisis de sentimiento
-    # Ejemplo de retorno: {Negative = 182, Neutral = 120, Positive = 278}
-    pass
+# Inicio de la conversación
+print("Bienvenido al chatbot de ayuda. ¿En qué puedo ayudarte?")
+chat.converse()
