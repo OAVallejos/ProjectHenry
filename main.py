fe609154c5792591d96d8e23022d7a7d970b6e6b
OAVallejos <135759800+OAVallejos@users.noreply.github.com>
2023-10-26 22:06:09 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-
 import pandas as pd
-
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware  # Importa el middleware de CORS
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# Agrega el middleware CORS antes de definir tus rutas
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Esto permite cualquier origen, debes ajustarlo para producción.
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -18,10 +15,10 @@ app.add_middleware(
 # Cargar los datos del archivo CSV al DataFrame
 data_games = pd.read_csv('limpio_games.csv')
 
-# Definir la función obtener_tabla_por_id 
+# Definir la función obtener_tabla_por_id
 def obtener_tabla_por_id(df, id):
     # Filtrar los juegos gratuitos (precio igual a 0) en el DataFrame con el ID proporcionado
-    juegos_gratuitos = df[df['price'] == 0]
+    juegos_gratuitos = df[(df['price'] == 0) & (df['developer'] == id)]
 
     # Extraer el año de lanzamiento (solo los últimos 2 dígitos de "YY")
     juegos_gratuitos['año_lanzamiento'] = juegos_gratuitos['release_date'].str[-2:]
@@ -35,12 +32,6 @@ def obtener_tabla_por_id(df, id):
 
     # Ordenar por año
     porcentaje_gratuitos_por_año = porcentaje_gratuitos_por_año.sort_index()
-
-    # Filtrar los juegos gratuitos (precio igual a 0) nuevamente
-    juegos_gratuitos = df[df['price'] == 0]
-
-    # Extraer el año de lanzamiento (solo los últimos 2 dígitos de "YY")
-    juegos_gratuitos['año_lanzamiento'] = juegos_gratuitos['release_date'].str[-2:]
 
     # Agrupar los juegos gratuitos por desarrollador y año de lanzamiento
     gratuitos_por_desarrollador = juegos_gratuitos.groupby(['developer', 'año_lanzamiento']).size().unstack(fill_value=0)
