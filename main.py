fb4ff8a9ba58fb1b6682f991a2e0a20ff0fdcbac
OAVallejos <135759800+OAVallejos@users.noreply.github.com>
2023-10-27 18:18:40 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -16,37 +16,52 @@ app.add_middleware(
 # Abre el archivo CSV
 data_games = pd.read_csv('limpio_games_gituno.csv')
 
-def obtener_informacion_desarrollador(data_games, id=None):
-    # Filtrar las filas con precio igual a 0 (contenido gratuito)
-    juegos_gratuitos = data_games[data_games['price'] == 0].copy()
+def encontrar_desarrollador_por_id(id, data_games):
+    # Filtrar el DataFrame para encontrar la fila con el ID proporcionado
+    fila = data_games[data_games['id'] == id]
 
-    # Extraer el año (en formato YYYY) de la columna 'release_date' y asignar a la copia original
-    juegos_gratuitos['release_year'] = juegos_gratuitos['release_date'].str.extract(r'(\d{4})')
-
-    if id is not None:
-        # Filtrar los juegos del desarrollador con el ID proporcionado
-        juegos_del_desarrollador = juegos_gratuitos[juegos_gratuitos['id'] == id]
-
-        # Agrupar por año y contar juegos gratuitos
-        agrupado = juegos_del_desarrollador.groupby(['release_year']).agg({'app_name': 'count'})
-
-        # Renombrar la columna
-        agrupado = agrupado.rename(columns={'app_name': 'juegos_gratuitos'})
-
-        # Calcular el porcentaje de juegos gratuitos
-        total_juegos = len(juegos_del_desarrollador)
-        agrupado['porcentaje_gratuitos'] = (agrupado['juegos_gratuitos'] / total_juegos) * 100
-
-        resultado_json = agrupado.to_dict(orient='split')
-        return resultado_json
+    if not fila.empty:
+        # Si se encuentra la fila, obtener el nombre del desarrollador asociado
+        desarrollador = fila['developer'].iloc[0]
+        return desarrollador
     else:
-        return {}  # Devolver un diccionario vacío si no se proporciona un ID
+        return "No se encontró un desarrollador para el ID proporcionado."
 
-# Crear un endpoint para obtener resultados por ID de desarrollador
+def developer(desarrollador, data_games):
+    
+    # Filtrar los elementos asociados a la empresa desarrolladora especificada
+    elementos_desarrollador = data_games[data_games['developer'] == desarrollador]
+
+    # Extraer el año (en formato YYYY) de la columna 'release_date'
+    elementos_desarrollador['release_year'] = elementos_desarrollador['release_date'].str.extract(r'(\d{4})')
+
+    # Calcular la cantidad de elementos lanzados por año
+    elementos_por_año = elementos_desarrollador.groupby('release_year').size().reset_index(name='Cantidad de Items')
+
+    # Calcular el porcentaje de elementos gratuitos por año
+    elementos_gratuitos_por_año = elementos_desarrollador[elementos_desarrollador['price'] == 0]
+    elementos_gratuitos_por_año = elementos_gratuitos_por_año.groupby('release_year').size().reset_index(name='Contenido Free en %')
+
+    # Fusionar ambos DataFrames para obtener el resultado final
+    resultado = elementos_por_año.merge(elementos_gratuitos_por_año, on='release_year', how='left')
+    resultado['Contenido Free en %'] = (resultado['Contenido Free en %'] / resultado['Cantidad de Items']) * 100
+
+    return resultado
+
+# Crear un endpoint para obtener resultados por ID
 @app.get("/obtener_resultados/{id}")
 async def obtener_resultados(id: int = None):
-    resultado = obtener_informacion_desarrollador(data_games, id)
-    if resultado:
-        return resultado
+    if id is None:
+        return {"mensaje": "Por favor, proporciona un ID válido."}
+    
+    nombre_del_desarrollador = encontrar_desarrollador_por_id(id, data_games)
+    
+    if nombre_del_desarrollador == "No se encontró un desarrollador para el ID proporcionado.":
+        return {"mensaje": nombre_del_desarrollador}
+    
+    resultado = developer(nombre_del_desarrollador, data_games)
+    
+    if not resultado.empty:
+        return resultado.to_dict(orient='split')
     else:
         return {"mensaje": "No se encontraron datos para el ID proporcionado."}
