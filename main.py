fbad7c9ddd84e1f066358d8c985f94030549c3d4
OAVallejos <135759800+OAVallejos@users.noreply.github.com>
2023-10-27 17:25:39 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -16,31 +16,37 @@ app.add_middleware(
 # Abre el archivo CSV
 data_games = pd.read_csv('limpio_games_gituno.csv')
 
-def obtener_informacion_desarrollador(data_games, developer_id):
+def obtener_informacion_desarrollador(data_games, id=None):
     # Filtrar las filas con precio igual a 0 (contenido gratuito)
     juegos_gratuitos = data_games[data_games['price'] == 0].copy()
 
     # Extraer el año (en formato YYYY) de la columna 'release_date' y asignar a la copia original
     juegos_gratuitos['release_year'] = juegos_gratuitos['release_date'].str.extract(r'(\d{4})')
 
-    # Filtrar los juegos del desarrollador con el ID proporcionado
-    juegos_del_desarrollador = juegos_gratuitos[juegos_gratuitos['developer'] == developer_id]
+    if id is not None:
+        # Filtrar los juegos del desarrollador con el ID proporcionado
+        juegos_del_desarrollador = juegos_gratuitos[juegos_gratuitos['id'] == id]
 
-    # Agrupar por año y contar juegos gratuitos
-    agrupado = juegos_del_desarrollador.groupby(['release_year']).agg({'app_name': 'count'})
+        # Agrupar por año y contar juegos gratuitos
+        agrupado = juegos_del_desarrollador.groupby(['release_year']).agg({'app_name': 'count'})
 
-    # Renombrar la columna
-    agrupado = agrupado.rename(columns={'app_name': 'juegos_gratuitos'})
+        # Renombrar la columna
+        agrupado = agrupado.rename(columns={'app_name': 'juegos_gratuitos'})
 
-    # Calcular el porcentaje de juegos gratuitos
-    total_juegos = len(juegos_del_desarrollador)
-    agrupado['porcentaje_gratuitos'] = (agrupado['juegos_gratuitos'] / total_juegos) * 100
+        # Calcular el porcentaje de juegos gratuitos
+        total_juegos = len(juegos_del_desarrollador)
+        agrupado['porcentaje_gratuitos'] = (agrupado['juegos_gratuitos'] / total_juegos) * 100
 
-    resultado_json = agrupado.to_dict(orient='split')
-    return resultado_json
+        resultado_json = agrupado.to_dict(orient='split')
+        return resultado_json
+    else:
+        return {}  # Devolver un diccionario vacío si no se proporciona un ID
 
 # Crear un endpoint para obtener resultados por ID de desarrollador
-@app.get("/obtener_resultados/{developer_id}")
-async def obtener_resultados(developer_id: str):
-    resultado = obtener_informacion_desarrollador(data_games, developer_id)
-    return resultado
+@app.get("/obtener_resultados/{id}")
+async def obtener_resultados(id: int = None):
+    resultado = obtener_informacion_desarrollador(data_games, id)
+    if resultado:
+        return resultado
+    else:
+        return {"mensaje": "No se encontraron datos para el ID proporcionado."}
