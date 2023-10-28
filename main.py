c48f5de958a8f0dda6bc9aa0861b94f68eaeb34e
OAVallejos <135759800+OAVallejos@users.noreply.github.com>
2023-10-27 21:22:04 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,80 +1,67 @@
 import pandas as pd
-import numpy as np
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-from typing import List
 
 app = FastAPI()
 
-# Middleware CORS
+# Agregar el middleware CORS antes de definir tus rutas
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Ajusta esto para la configuración de producción
+    allow_origins=["*"],  # Esto permite cualquier origen, debes ajustarlo para producción.
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Cargar el archivo CSV una vez al iniciar la aplicación
-try:
-    data_games = pd.read_csv('limpio_games_gituno.csv')
-except Exception as e:
-    raise Exception("Se produjo un error al leer el archivo CSV: " + str(e))
+# Abre el archivo CSV
+data_games = pd.read_csv('limpio_games_gituno.csv')
 
-# Función para buscar el desarrollador por ID
-def encontrar_desarrollador_por_id(id, data):
-    # Convertir id a un número
-    id = int(id)
-    
-    fila = data[data['id'] == id]
+def encontrar_desarrollador_por_id(id, data_games):
+    # Filtrar el DataFrame para encontrar la fila con el ID proporcionado
+    fila = data_games[data_games['id'] == id]
+
     if not fila.empty:
-        return fila['developer'].iloc[0]
-    return None
+        # Si se encuentra la fila, obtener el nombre del desarrollador asociado
+        desarrollador = fila['developer'].iloc[0]
+        return desarrollador
+    else:
+        return "No se encontró un desarrollador para el ID proporcionado."
 
-# Función para procesar datos del desarrollador
-def procesar_desarrollador(desarrollador, data):
-    elementos_desarrollador = data[data['developer'] == desarrollador]
-    if elementos_desarrollador.empty:
-        return {"mensaje": "No se encontraron elementos para el desarrollador proporcionado."}
+def developer(desarrollador, data_games):
+    
+    # Filtrar los elementos asociados a la empresa desarrolladora especificada
+    elementos_desarrollador = data_games[data_games['developer'] == desarrollador]
 
+    # Extraer el año (en formato YYYY) de la columna 'release_date'
     elementos_desarrollador['release_year'] = elementos_desarrollador['release_date'].str.extract(r'(\d{4})')
-    
+
+    # Calcular la cantidad de elementos lanzados por año
     elementos_por_año = elementos_desarrollador.groupby('release_year').size().reset_index(name='Cantidad de Items')
-    
+
+    # Calcular el porcentaje de elementos gratuitos por año
     elementos_gratuitos_por_año = elementos_desarrollador[elementos_desarrollador['price'] == 0]
     elementos_gratuitos_por_año = elementos_gratuitos_por_año.groupby('release_year').size().reset_index(name='Contenido Free en %')
-    
+
+    # Fusionar ambos DataFrames para obtener el resultado final
     resultado = elementos_por_año.merge(elementos_gratuitos_por_año, on='release_year', how='left')
     resultado['Contenido Free en %'] = (resultado['Contenido Free en %'] / resultado['Cantidad de Items']) * 100
 
-    # Verificar si el resultado es None
-    if resultado is None:
-        return {"mensaje": "No se pudo calcular el resultado para el desarrollador proporcionado."}
-
-    # Reemplazar NaN e Infinity con None
-    resultado = resultado.where(pd.notnull(resultado), None)
-    resultado = resultado.replace([np.inf, -np.inf], None)
-
     return resultado
 
-# Modelo de datos para la solicitud
-class Item(BaseModel):
-    value: float
-
-# Modificada la función "calculate" para verificar el rango de los valores
-@app.post("/calculate")
-def calculate(items: List[Item]):
-    # Verificar el rango de los valores antes de llamar a perform_calculation
-    if not items:
-        raise HTTPException(status_code=400, detail="La lista de items está vacía")
+# Crear un endpoint para obtener resultados por ID
+@app.get("/obtener_resultados/{id}")
+async def obtener_resultados(id: int = None):
+    if id is None:
+        return {"mensaje": "Por favor, proporciona un ID válido."}
     
-    for item in items:
-        if item.value < -3.4e38 or item.value > 3.4e38:
-            raise HTTPException(status_code=400, detail="Invalid input data: Value is out of range")
+    nombre_del_desarrollador = encontrar_desarrollador_por_id(id, data_games)
     
-    try:
-        result = perform_calculation(items)
-        return {"result": result}
-    except ValueError as ve:
-        return {}
+    if nombre_del_desarrollador == "No se encontró un desarrollador para el ID proporcionado.":
+        return {"mensaje": nombre_del_desarrollador}
+    
+    resultado = developer(nombre_del_desarrollador, data_games)
+    
+    if not resultado.empty:
+        return resultado.to_dict(orient='split')
+    else:
+        return {"mensaje": "No se encontraron datos para el ID proporcionado."}
