ec55050f340315de75df524e0db7a3758cf25b7c
OAVallejos <135759800+OAVallejos@users.noreply.github.com>
2023-10-30 00:07:01 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Dict
 import json
-
+from pydantic import BaseModel
 import logging
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
